4250a161501dc75ca57e49029ec35812326f0952
SnowyField <aswdqe1x@gmail.com>
2022-05-28 18:58:28 +0700

    thuan

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        My Blockchain Application!
+      </h1>
+    </div>
+  )
+}
+export default App
