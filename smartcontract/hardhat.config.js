bee31a9b5577aa4ffa13f60b7b6c077541a5b744
SnowyField <aswdqe1x@gmail.com>
2022-07-12 20:25:26 +0700

    thuan

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,21 +1,10 @@
-require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity : '0.8.0',
+  networks : {
+    ropsten:{
+      url: `https://eth-rinkeby.alchemyapi.io/v2/Ufvjrdj1BIlCIHBCH2Tn8hGeNFm-98A-`,
+      accounts: ['e657d4ea627fa889d81f291a79a631f5f7eedf76e8c8ed1f38ca667c85135c47']
+    } 
+  }
+}
--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x71A7464FA7b0FDEf51745cD04efcBE4F1484CE4c';
+export const contractAddress = '0x8D4d08a4E408352D7637b510afe1666A0C1b4B3B';
